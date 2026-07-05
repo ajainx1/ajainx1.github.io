ce65935c45a3ea5a43249e3d528767796649a422
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:11:25 +0530

    clean: prune unwanted backend file noc/alert/check_api_spawn3.php from static build

--- noc/alert/check_api_spawn3.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$lines = file("C:\\inetpub\\wwwroot\\alert\\api.php");
-foreach ($lines as $i => $line) {
-    if (strpos($line, "start /B") !== false) {
-        echo "Line $i: $line";
-    }
-}
-?>
