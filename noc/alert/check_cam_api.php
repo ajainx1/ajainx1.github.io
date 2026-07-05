0cd62b521c0d437ff469b2e127851085fecb0e60
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:08:32 +0530

    feat: add sanitized static noc/alert/check_cam_api.php for professional portfolio

---
+++ noc/alert/check_cam_api.php
@@ -0,0 +1,8 @@
+<?php
+$context = stream_context_create([
+    "http" => [
+        "header" => "Authorization: Digest username=\"admin\", realm=\"Login to 10.133.15.18\", nonce=\"\", uri=\"/cgi-bin/configManager.cgi?action=getConfig&name=VideoWidget\", response=\"\""
+    ]
+]);
+echo @file_get_contents("http://10.133.15.18/cgi-bin/configManager.cgi?action=getConfig&name=VideoWidget", false, $context);
+?>
