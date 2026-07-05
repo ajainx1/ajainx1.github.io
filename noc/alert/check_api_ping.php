7e904c495e5afe5a1f20ce2394210141310841ca
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:07:44 +0530

    feat: add sanitized static noc/alert/check_api_ping.php for professional portfolio

---
+++ noc/alert/check_api_ping.php
@@ -0,0 +1,11 @@
+<?php 
+$lines = file("C:\\inetpub\\wwwroot\\alert\\api.php");
+$start = 0;
+foreach ($lines as $i => $line) {
+    if (strpos($line, "function run_parallel_ping_cycle") !== false) {
+        $start = $i;
+        break;
+    }
+}
+echo implode("", array_slice($lines, $start, 35));
+?>
