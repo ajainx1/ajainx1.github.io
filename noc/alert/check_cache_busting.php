ca7c20c2fa9c3bf3232d5178697865b25016fa54
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:08:30 +0530

    feat: add sanitized static noc/alert/check_cache_busting.php for professional portfolio

---
+++ noc/alert/check_cache_busting.php
@@ -0,0 +1,8 @@
+<?php 
+$lines = file("C:\\inetpub\\wwwroot\\alert\\static\\script.js");
+foreach ($lines as $i => $line) {
+    if (strpos($line, "endpoint=status") !== false) {
+        echo "Line $i: $line";
+    }
+}
+?>
