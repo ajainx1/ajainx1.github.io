c11d70b13800cc40531eb6fb982b564a9f86613a
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:07:58 +0530

    feat: add sanitized static noc/alert/check_api_spawn_native.php for professional portfolio

---
+++ noc/alert/check_api_spawn_native.php
@@ -0,0 +1,11 @@
+<?php 
+$lines = file("C:\\inetpub\\wwwroot\\alert\\api.php");
+$start = 0;
+foreach ($lines as $i => $line) {
+    if (strpos($line, "native_poller.py") !== false) {
+        $start = $i;
+        break;
+    }
+}
+echo implode("", array_slice($lines, max(0, $start - 5), 10));
+?>
