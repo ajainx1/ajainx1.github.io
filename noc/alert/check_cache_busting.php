9257ea17c063bd134e3d56972d1222ceb9e8dccf
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:11:47 +0530

    clean: prune unwanted backend file noc/alert/check_cache_busting.php from static build

--- noc/alert/check_cache_busting.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$lines = file("C:\\inetpub\\wwwroot\\alert\\static\\script.js");
-foreach ($lines as $i => $line) {
-    if (strpos($line, "endpoint=status") !== false) {
-        echo "Line $i: $line";
-    }
-}
-?>
