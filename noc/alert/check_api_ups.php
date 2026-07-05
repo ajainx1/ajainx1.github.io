c2a2abf900185e85adad6d54273c1d5f562cdad8
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:11:44 +0530

    clean: prune unwanted backend file noc/alert/check_api_ups.php from static build

--- noc/alert/check_api_ups.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$json = file_get_contents("http://127.0.0.1/alert/api.php");
-$data = json_decode($json, true);
-foreach ($data["hosts"] as $h) {
-    if ($h["ip"] === "10.133.15.42" || $h["ip"] === "10.133.15.45") {
-        echo json_encode($h) . "\n";
-    }
-}
-?>
