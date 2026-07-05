f6fd595438eebf72a7c81d991d07a2e8ecfe6118
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:07:52 +0530

    feat: add sanitized static noc/alert/check_api_spawn.php for professional portfolio

---
+++ noc/alert/check_api_spawn.php
@@ -0,0 +1,8 @@
+<?php 
+$lines = file("C:\\inetpub\\wwwroot\\alert\\api.php");
+foreach ($lines as $i => $line) {
+    if (strpos($line, "--run_ping_cycle") !== false && strpos($line, "php_sapi_name") === false && strpos($line, "\$argv") === false) {
+        echo "Line $i: $line";
+    }
+}
+?>
