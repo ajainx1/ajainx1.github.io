8a97ec963acb7f1883e63a55326e8a2b921ca7e0
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:08:34 +0530

    feat: add sanitized static noc/alert/check_categories.php for professional portfolio

---
+++ noc/alert/check_categories.php
@@ -0,0 +1,11 @@
+<?php 
+$json = file_get_contents("C:\\inetpub\\wwwroot\\alert\\config.json");
+$data = json_decode($json, true);
+$categories = [];
+foreach ($data["hosts"] as $h) {
+    if (isset($h["category"])) {
+        $categories[$h["category"]] = true;
+    }
+}
+print_r(array_keys($categories));
+?>
