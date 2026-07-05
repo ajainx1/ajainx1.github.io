b8197f253ddf0c003c3c6cd55cd05e0af455f2c8
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:12:07 +0530

    clean: prune unwanted backend file noc/alert/check_getpip.php from static build

--- noc/alert/check_getpip.php
+++
@@ -1,4 +0,0 @@
-<?php 
-pclose(popen("start /b cmd /c \"C:\\inetpub\\wwwroot\\alert\\python\\python.exe C:\\inetpub\\wwwroot\\alert\\python\\get-pip.py > C:\\inetpub\\wwwroot\\alert\\getpip_error.log 2>&1\"", "r"));
-echo "SPAWNED"; 
-?>
