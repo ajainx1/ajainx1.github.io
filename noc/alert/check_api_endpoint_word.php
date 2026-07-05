0660b06bd0c56bcc4a662acccf710b2357a01a12
Aditya Jain <aavkjain@hotmail.com>
2026-07-06 00:07:28 +0530

    feat: add sanitized static noc/alert/check_api_endpoint_word.php for professional portfolio

---
+++ noc/alert/check_api_endpoint_word.php
@@ -0,0 +1,11 @@
+<?php 
+$content = file_get_contents("C:\\inetpub\\wwwroot\\alert\\api.php");
+if (strpos($content, "endpoint") !== false) {
+    echo "Found endpoint";
+} else {
+    echo "NO ENDPOINT";
+}
+if (strpos($content, "action") !== false) {
+    echo "\nFound action";
+}
+?>
