2ad398400fc1fbc7c684b3c6a44b62c3305c3ef7
Aditya Jain <aavkjain@hotmail.com>
2026-08-14 17:11:42 +0530

    feat: deploy Trinity Seal favicon stack (paw shield SVG, apple-touch-icon, icon-192, icon-512, webmanifest)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,9 +93,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <meta name="theme-color" content="#070b14" />
+        <meta name="theme-color" content="#070B14" />
         <meta name="google-adsense-account" content="ca-pub-6072468142870937" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
